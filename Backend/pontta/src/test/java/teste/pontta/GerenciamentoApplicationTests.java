eeb841f7eb607112847d1857e5b1e8445da6bdb8
FelixVillanuevaCondori <felixeduardobx@gmail.com>
2025-11-10 14:34:56 -0300

    Firts Commit

---
+++ Backend/pontta/src/test/java/teste/pontta/GerenciamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package teste.pontta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
